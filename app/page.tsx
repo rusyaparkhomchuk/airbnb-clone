14912e75d6fa36192ad92b93be5ef7a069bae1d0
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2023-11-09 22:26:03 +0200

    final fix of page.tsx not rendering statically error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Container from '@/app/components/Container'
-import ListingCard from '@/app/components/listings/ListingCard'
-import EmptyState from '@/app/components/EmptyState'
+export const dynamic = 'force-dynamic'
 
-import getListings, { IListingsParams } from '@/app/actions/getListings'
-import getCurrentUser from '@/app/actions/getCurrentUser'
-import ClientOnly from './components/ClientOnly'
+import getCurrentUser from './actions/getCurrentUser'
+import getListings, { IListingsParams } from './actions/getListings'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
+import ListingCard from './components/listings/ListingCard'
 
 interface HomeProps {
 	searchParams: IListingsParams
@@ -15,39 +15,21 @@ const Home = async ({ searchParams }: HomeProps) => {
 	const currentUser = await getCurrentUser()
 
 	if (listings.length === 0) {
-		return (
-			<ClientOnly>
-				<EmptyState showReset />
-			</ClientOnly>
-		)
+		return <EmptyState showReset />
 	}
 
 	return (
-		<ClientOnly>
-			<Container>
-				<div
-					className='
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          '
-				>
-					{listings.map((listing: any) => (
-						<ListingCard
-							currentUser={currentUser}
-							key={listing.id}
-							data={listing}
-						/>
-					))}
-				</div>
-			</Container>
-		</ClientOnly>
+		<Container>
+			<div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 '>
+				{listings.map((listing) => (
+					<ListingCard
+						key={listing.id}
+						currentUser={currentUser}
+						data={listing}
+					/>
+				))}
+			</div>
+		</Container>
 	)
 }
 
